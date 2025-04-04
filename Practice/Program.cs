1b8522c53314399410d079af6027558d791622bc
devanshgupta0110 <55200395+devanshgupta0110@users.noreply.github.com>
2025-04-04 20:47:08 +0200

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace variables
+{
+	class demo
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("enter value of n");
+			int n = Convert.ToInt32(Console.ReadLine());
+			for (int i = n; i >= 1; i--)
+
+				for (int j = 1; j <= i; j++)
+				{
+					Console.Write(j);
+				}
+		}
+		Console.ReadLine();  
+        }
+}  
+}  
